ebeeb085e753751218f8b854717c69ff0f09b84c
Nicolas Calvo <ncalvo@paypal.com>
2020-12-14 09:54:53 -0300

    fixed

--- detecthttp/pytest.py
+++ detecthttp/pytest.py
@@ -68,12 +68,17 @@ def pytest_runtest_call(item):
 
     yield
 
-    if enabled:
-        cassette_manager.__exit__(*sys.exc_info())
-        if len(cassette):
-            error_report = UnmockedRequestsDetected(cassette)
-            item.config._detecthttp_reports[item.nodeid] = error_report
-
+if enabled:
+    cassette_manager.__exit__(*sys.exc_info())
+    requested_host = []
+    for request in cassette.data:
+        domain = request[0].host.replace('www.', '')  # remove www if exists
+        host = domain.split('.')[0]  # get host only no .com/.biz/.org
+        requested_host.append(host)
+    non_ignored_or_mocked_host = [host for host in requested_host if host not in item.config._detecthttp_ignored_hosts]
+    if non_ignored_or_mocked_host:
+        error_report = UnmockedRequestsDetected(cassette)
+        item.config._detecthttp_reports[item.nodeid] = error_report
 
 def pytest_runtest_teardown(item):
     # Note unmocked interactions collected during runtest_call.
